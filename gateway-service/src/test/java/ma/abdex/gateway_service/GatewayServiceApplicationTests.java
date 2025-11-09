eb9e79cca6669453fcdf7350e95452de8143a189
Abdex <abdex4social@gmail.com>
2025-11-09 01:34:17 +0100

    Implement gateway, discovery and config services

--- gateway-service/src/test/java/ma/abdex/gateway_service/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/ma/abdex/gateway_service/GatewayServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package ma.abdex.gatewayservice;
+package ma.abdex.gateway_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GatewayServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
